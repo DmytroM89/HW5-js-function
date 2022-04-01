b27d4b7f545bc0d1f03c575256f198fa8519f2d9
MartynenkoDV <MartynenkoDV@comfy.ua>
2022-04-02 00:38:27 +0300

    editing HW5-js-function

--- script.js
+++ script.js
@@ -26,31 +26,56 @@ function showNumsHard(a, b) {
 }
 showNumsHard(1, -1);
 
-log('-------------- # 5 Часы на экране')
-function clockInit() {
+log('-------------- # 5 Часы на экране - Первый вариант')
+const clock = document.getElementById('clock');
+let formatFull = true;
+let time = '';
+
+/*function clockInit() {
     const date = new Date();
     let hours = date.getHours();
     if (hours < 10) {
         hours = '0' + hours;
     }
-    document.getElementById('hour').innerHTML = hours;
 
     let minutes = date.getMinutes();
     if (minutes < 10) {
         minutes = '0' + minutes;
     }
-    document.getElementById('min').innerHTML = minutes;
 
     let seconds = date.getSeconds();
     if (seconds < 10) {
         seconds = '0' + seconds;
     }
-    document.getElementById('sec').innerHTML = seconds;
+
+    //clock.innerHTML = formatFull ? hours + ':' + minutes + ':' + seconds : hours + ':' + minutes; // как вариант
+    if (formatFull) {
+        time = hours + ':' + minutes + ':' + seconds;
+    } else {
+        time = hours + ':' + minutes;
+    }
+    clock.innerHTML = time;
+}*/
+
+log('-------------- # 5 Часы на экране - Второй вариант')
+/**
+ * Что здесь происходит?
+ * new Date().toTimeString - получаем время формата '00:07:55 GMT+0300 (Восточная Европа, летнее время)'
+ * new Date().toTimeString.split(' ')[0] - разбиваем строку на массив строк и по нулевому индексу получаем время в полном формате
+ */
+function clockInit() {
+    if (formatFull) {
+        time = new Date().toTimeString().split(' ')[0];
+    } else {
+        time = new Date().toTimeString().split(' ')[0].substring(0,5); // то же, что и выше + substring(0,5) обрезаем двоеточие с секундами
+    }
+    clock.innerHTML = time;
 }
 
 setInterval(clockInit, 1000);
 
-// Часы Toggle - “full/short” format
-function changeFormat() {
-    document.getElementById('clock').classList.toggle('short');
-}
+// Toggle time format
+clock.addEventListener('click', () => {
+    formatFull = !formatFull;
+    clockInit(); // вызываем ф-цию сразу, чтобы не ждать
+});
